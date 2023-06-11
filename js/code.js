a2aaa8b04cc934941883f911029265e3fee5d8b4
KengiiGMs <kaikiaoto22@gmail.com>
2023-06-10 21:57:56 -0300

    Capturando os dados dos radios com JS

--- js/code.js
+++ js/code.js
@@ -1,9 +1,27 @@
-const toastTrigger = document.getElementById("AdicionarCarrinhoBtn");
-const toastLiveExample = document.getElementById("liveToast");
-
-if (toastTrigger) {
+function alerta() {
+	const toastLiveExample = document.getElementById("liveToast");
 	const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-	toastTrigger.addEventListener("click", () => {
-		toastBootstrap.show();
-	});
+	toastBootstrap.show();
+}
+
+function addCarrinho() {
+	event.preventDefault();
+	let sucessoCor = false;
+	let sucessoTamanho = false;
+	let cores = document.getElementsByName("cores");
+	for (var radio of cores) {
+		if (radio.checked) {
+			sucessoCor = true;
+		}
+	}
+	let tamanho = document.getElementsByName("tamanhos");
+	for (var radio of tamanho) {
+		if (radio.checked) {
+			sucessoTamanho = true;
+		}
+	}
+
+	if (sucessoCor == true && sucessoTamanho == true) {
+		alerta();
+	}
 }
